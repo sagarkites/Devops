2f413a36a269d7f2a748a72ec5ae8deaa2b5dfbf
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2018-08-31 16:55:08 +0530

    Update LambdaInstance.py

--- botoscripts/LambdaInstance.py
+++ botoscripts/LambdaInstance.py
@@ -1,10 +1,8 @@
 import boto3
-
-def lambda_handler(event, context):
-    ec2 = boto3.resource('ec2')
-    instance = ec2.create_instances(
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
          ImageId='ami-b70554c8',
          MinCount=1,
          MaxCount=1,
          InstanceType='t2.micro')
-    print instance[0].id
+print instance[0].id
