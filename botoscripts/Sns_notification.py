ac33f381717631fcfe7cc7cf2b9012f6904522b4
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2018-08-31 16:49:57 +0530

    Update Sns_notification.py

--- botoscripts/Sns_notification.py
+++ botoscripts/Sns_notification.py
@@ -3,8 +3,8 @@ ec2 = boto3.client('ec2')
 sns = boto3.client('sns')
 list = ec2.describe_instances()
 for i in list["Reservations"]:
-    j = [sagar["InstanceId"] for sagar in i["Instances"]]
-    k = [chanti["State"] for chanti in i["Instances"]]
+    j = [instance["InstanceId"] for instance in i["Instances"]]
+    k = [response["State"] for response in i["Instances"]]
     l = k[0]
     if l["Name"] == "running":
         shutdown = (ec2.stop_instances(InstanceIds=j))
