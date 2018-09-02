ee31071cdafb9f77d1ea7ee99be0161f458f0a8a
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2018-09-02 19:41:26 +0530

    update file

--- botoscripts/Lambda_scripts/List_ec2Instances.py
+++ botoscripts/Lambda_scripts/List_ec2Instances.py
@@ -3,7 +3,7 @@ def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
     list = ec2.describe_instances()
     for i in list["Reservations"]:
-        j = [sagar["InstanceId"] for sagar in i["Instances"]]
-        k = [chanti["State"] for chanti in i["Instances"]]
+        j = [ver_1["InstanceId"] for ver_1 in i["Instances"]]
+        k = [ver_2["State"] for ver_2 in i["Instances"]]
         l = k[0]
         print("The Instance_id {0} is {1}".format(j,l["Name"]))
