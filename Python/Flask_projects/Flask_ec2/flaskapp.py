87032ff24826e722d98da4381d9ab4be109186fc
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2018-12-20 08:27:58 +0530

    upadted

--- Python/Flask_projects/Flask_ec2/flaskapp.py
+++ Python/Flask_projects/Flask_ec2/flaskapp.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+# Decorator
 @app.route('/')
 def hello_world():
-  return render_template('movie.html')
+# Setting Html file in script  
+  return render_template('movie.html') 
 
 if __name__ == '__main__':
   app.run()
