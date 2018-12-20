8580f9bcecfebbe73cb21858eb96a4d29d7df38a
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2018-12-20 11:42:19 +0530

    update

--- Python/Flask_projects/Flask_ec2/flaskapp.py
+++ Python/Flask_projects/Flask_ec2/flaskapp.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
-# Decorator
-@app.route('/')
+
+@app.route('/movie')
 def hello_world():
-# Setting Html file in script  
-  return render_template('movie.html') 
+  return render_template('movie.html')
+
+@app.route('/sagar')
+def iam():
+    return 'Think Globally Act Locally'
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'sagar' or request.form['password'] != 'sagar215':
+            error = 'Invalid'
+        else:
+            return redirect(url_for('hello_world'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
   app.run()
+
